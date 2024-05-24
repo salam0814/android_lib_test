90d9f17788aa9c45112fe745b6921cb3e686794d
SalamQ <abudu163163@163.com>
2024-05-24 11:43:16 +0800

    test

---
+++ src/main/java/com/prj/android/lib/StringTest.java
@@ -0,0 +1,11 @@
+package com.prj.android.lib;
+
+/**
+ * @Author: MuMu
+ * @Date: 2024/5/24 10:47
+ */
+public class StringTest {
+    public String test() {
+        return "test";
+    }
+}
---
+++ src/main/java/com/prj/android/lib/TT.java
@@ -0,0 +1,8 @@
+package com.prj.android.lib;
+
+/**
+ * @Author: MuMu
+ * @Date: 2024/5/24 10:48
+ */
+public class TT {
+}
